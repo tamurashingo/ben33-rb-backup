b22a45d0b0111a2226ed5378885c1a4f3cd7b23b
tamura shingo <tamura.shingo@gmail.com>
2018-01-29 00:53:10 +0900

    BASIC認証の追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
   def index
     @events = Event.all
   end
